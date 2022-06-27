2ca523b0790ff5ad5e86533857c0b0412e650b30
RonicFlamer <ronicflamer@gmail.com>
2022-06-26 19:58:02 -0700

    fixed double break

--- ProductManager.cc
+++ ProductManager.cc
@@ -8,7 +8,6 @@ void ProductManager::removeProduct(Product *p, std::string name) {
     for(int i = 0; i < currentProducts.size(); i++) {
         if(p->getName() == name){
             currentProducts.erase(currentProducts.begin()+i);
-            break;
             break; 
         }
     }
